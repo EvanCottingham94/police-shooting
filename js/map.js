cbc78a895b2222855f5ec046832c2339ecb96f8d
Evan Cottingham <ejc1488@gmail.com>
2015-10-19 01:38:49 -0700

    made some changes

--- js/map.js
+++ js/map.js
@@ -11,8 +11,8 @@ var getData = function(map) {
 	 $.ajax({
 		 url:"data/response.json",
 		 type: "get",
-		 success:function(data, map) {
-		   //data = data
+		 success:function(data) {
+		   data = data
 		   customBuild(data, map);
 		 }, 
 		 dataType:"json"
@@ -22,18 +22,29 @@ var getData = function(map) {
 // Loop through your data and add the appropriate layers and points
 var customBuild = function(data, map) {
 	// Be sure to add each layer to the map
-	var arrayLayers;
-	var race;
+	var arrayLayers = new Array();
+	var race = new L.LayerGroup([]);
 	for(i = 0; i < data.length; i++) {
-		if($.inArray(data[i].race, arrayLayers) != -1) {
-			arrayLayers.push(data[i].race);
-			race = new L.LayerGroup([data[i].race]);
+		arrayLayers.push(data[i].race);
+		if($.inArray(data[i].race, arrayLayers) == -1) {	
+			race.addLayer(data[i].race);
 			race.addTo(map);
 	    }
-		var circle = new L.circleMarker(data[i].lat, data[i].lng);
-		circle.addTo(race);
+		if(data[i]["Victim's Gender"] == "Male"){
+			var circle = L.circleMarker([data[i].lat, data[i].lng], { color: "blue", fillColor: "blue", radius: 5, opacity: 1}).addTo(map);
+			circle.bindPopup(data[i].Summary);
+		} else {
+			var circle = L.circleMarker([data[i].lat, data[i].lng], { color: "red", fillColor: "red", radius: 5, opacity: 1}).addTo(map);
+			circle.bindPopup(data[i].Summary);
+		}
 	}
+	
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
+	var baseMaps = {
+		"Unknown" : race.Unknown
+	}
+	
+	var control = L.control.layers(baseMaps).addTo(map);
 }
 
 
