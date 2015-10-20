7334f58b0bb2ac7df2af278e768aa3748eba90f0
Evan Cottingham <ejc1488@gmail.com>
2015-10-19 22:53:34 -0700

    finish

--- js/map.js
+++ js/map.js
@@ -1,4 +1,9 @@
 // Function to draw your map
+var countMen = 0;
+var countWomen = 0;
+var countUnarmed = 0;
+var countArmed = 0;
+
 var drawMap = function() {
 	var map = L.map('container').setView([39.10, -95.80], 4);
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
@@ -22,29 +27,40 @@ var getData = function(map) {
 // Loop through your data and add the appropriate layers and points
 var customBuild = function(data, map) {
 	// Be sure to add each layer to the map
-	var arrayLayers = new Array();
-	var race = new L.LayerGroup([]);
+	var raceLay = {};
+	
 	for(i = 0; i < data.length; i++) {
-		arrayLayers.push(data[i].race);
-		if($.inArray(data[i].race, arrayLayers) == -1) {	
-			race.addLayer(data[i].race);
-			data[i].race.addTo(map);
-	    }
+		if(data[i].Race == undefined){
+				data[i].Race = "Unknown";
+			}
+		if(raceLay[data[i].Race] == undefined){
+			raceLay[data[i].Race] = new L.LayerGroup([]);
+			raceLay[data[i].Race].addTo(map);
+		}
 		if(data[i]["Victim's Gender"] == "Male"){
-			var circle = L.circleMarker([data[i].lat, data[i].lng], { color: "blue", fillColor: "blue", radius: 5, opacity: 1}).addTo(map);
+			var circle = L.circleMarker([data[i].lat, data[i].lng], { color: "blue", fillColor: "blue", radius: 5, opacity: 1}).addTo(raceLay[data[i].Race]);
 			circle.bindPopup(data[i].Summary);
-		} else {
-			var circle = L.circleMarker([data[i].lat, data[i].lng], { color: "red", fillColor: "red", radius: 5, opacity: 1}).addTo(map);
+			countMen++;
+		} else if(data[i]["Victim's Gender"] == "Female"){
+			var circle = L.circleMarker([data[i].lat, data[i].lng], { color: "red", fillColor: "red", radius: 5, opacity: 1}).addTo(raceLay[data[i].Race]);
 			circle.bindPopup(data[i].Summary);
+			countWomen++;
+		} 
+		if(data[i]["Armed or Unarmed?"] == "Unarmed") {
+			countUnarmed++;
+		} else if(data[i]["Armed or Unarmed?"] == "Armed") {
+			countArmed++;
 		}
 	}
-	
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
-	var raceLay = {
-		"White" : race
-	}
-	
 	var control = L.control.layers(null, raceLay).addTo(map);
+	table(countMen, countWomen, countUnarmed, countArmed);
+	
 }
 
-
+var table = function(countMen, countWomen, countUnarmed, countArmed) { 
+	if ($("#myTable tbody").length == 0) { 
+		$("#myTable").append("<tbody></tbody>"); 
+	}
+	$("#myTable tbody").append( "<tr>" + "<td>" + countMen + "</td>" + "<td>" + countWomen + "</td>" + "<td>" + countUnarmed + "</td>" + "<td>" + countArmed + "</td>" + "</tr>" ); 
+}
